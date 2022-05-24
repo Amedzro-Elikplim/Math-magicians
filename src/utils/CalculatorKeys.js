5bc5231768db1fa74d85bd9c8ce1c8718f33fbc6
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-05-24 22:58:47 +0000

    Add keys

---
+++ src/utils/CalculatorKeys.js
@@ -0,0 +1,24 @@
+const calculatorKeys = [
+  { key: 0, className: 'grey-bg' },
+  { key: 'AC', className: 'white-bg' },
+  { key: '+/-', className: 'white-bg' },
+  { key: '%', className: 'white-bg' },
+  { key: '+', className: 'orange-bg' },
+  { key: '7', className: 'white-bg' },
+  { key: '8', className: 'white-bg' },
+  { key: '9', className: 'white-bg' },
+  { key: 'x', className: 'orange-bg' },
+  { key: '4', className: 'white-bg' },
+  { key: '5', className: 'white-bg' },
+  { key: '6', className: 'white-bg' },
+  { key: '-', className: 'orange-bg' },
+  { key: '1', className: 'white-bg' },
+  { key: '2', className: 'white-bg' },
+  { key: '3', className: 'white-bg' },
+  { key: '+', className: 'orange-bg' },
+  { key: '0', className: 'white-bg' },
+  { key: '.', className: 'white-bg span-2' },
+  { key: '=', className: 'orange-bg' },
+];
+
+export default calculatorKeys;
