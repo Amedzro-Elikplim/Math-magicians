3709303e428f5be72f3483be091cf38f4a94c194
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-05-25 00:29:04 +0000

    Position key 7 below key ÷

--- src/utils/CalculatorKeys.js
+++ src/utils/CalculatorKeys.js
@@ -1,9 +1,9 @@
 const calculatorKeys = [
-  { key: 0, className: 'gray-bg' },
+  { key: '0.00', className: 'gray-bg' },
   { key: 'AC', className: 'white-bg' },
   { key: '+/-', className: 'white-bg' },
   { key: '%', className: 'white-bg' },
-  { key: '+', className: 'orange-bg' },
+  { key: '÷', className: 'orange-bg' },
   { key: 7, className: 'white-bg' },
   { key: 8, className: 'white-bg' },
   { key: 9, className: 'white-bg' },
@@ -16,8 +16,8 @@ const calculatorKeys = [
   { key: 2, className: 'white-bg' },
   { key: 3, className: 'white-bg' },
   { key: '+', className: 'orange-bg' },
-  { key: 0, className: 'white-bg' },
-  { key: '.', className: 'white-bg span-2' },
+  { key: 0, className: 'white-bg span-2' },
+  { key: '.', className: 'white-bg' },
   { key: '=', className: 'orange-bg' },
 ];
 
