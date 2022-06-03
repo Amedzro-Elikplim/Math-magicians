427c744b39400d9788fe50d755c53d9a2512b224
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-03 21:57:48 +0000

    Add nav links to navbar

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -1,8 +1,19 @@
+import '../../styles/Nav.css';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
-    <nav>nav</nav>
+    <nav className="d-flex">
+      <h3>Math Magician</h3>
+      <div>
+        <ul>
+          <Link to="/">Home</Link>
+          <Link to="/calculate">Calculator</Link>
+          <Link to="/about">Quote</Link>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
