a064c43e0463acee53c9d895b9b6ab7fbc10cbe2
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-03 23:30:06 +0000

    Add quote and style using flexbox

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -1,8 +1,14 @@
+import '../styles/Quote.css';
 import React from 'react';
 
 function About() {
   return (
-    <div>About page</div>
+    <div className="quote d-flex">
+      <p>
+        Mathematics is not about numbers, equations, computations, or
+        algorithms: it is about understanding. - William Paul Th urston
+      </p>
+    </div>
   );
 }
 
