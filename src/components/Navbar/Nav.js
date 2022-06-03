cd35dd2fa97f39b4d5b30d968db3721ef66bb288
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-03 22:31:08 +0000

    Add navlinks and their routes

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -4,15 +4,13 @@ import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
-    <nav className="d-flex">
+    <nav className="nav d-flex">
       <h3>Math Magician</h3>
-      <div>
-        <ul>
-          <Link to="/">Home</Link>
-          <Link to="/calculate">Calculator</Link>
-          <Link to="/about">Quote</Link>
-        </ul>
-      </div>
+      <ul className="nav-links d-flex">
+        <Link to="/">Home</Link>
+        <Link to="/calculate">Calculator</Link>
+        <Link to="/about">Quote</Link>
+      </ul>
     </nav>
   );
 }
